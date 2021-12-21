085f30349e6c577d6f71e3c9d955e4b249977dfa
dir18 <dirlyzandrea30@gmail.com>
2021-12-21 10:20:33 -0300

    arreglos en formularios

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(express.static(publicPath));
 app.get('/', (req, res) => {
     let htmlPath = path.resolve(__dirname,'./views/home.html')
     res.sendFile(htmlPath)
-})
+});
 
+app.get('/register', (req, res) => {
+    let htmlPath1 = path.resolve(__dirname,'./views/register.html')
+    res.sendFile(htmlPath1)
+});
+
+app.get('/login', (req, res) => {
+    let htmlPath2 = path.resolve(__dirname, './views/login.html')
+    res.sendFile(htmlPath2)
+})
 
